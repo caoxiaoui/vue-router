4896fc22ddcd5f5d4ec959f501d781b091145d51
caoxiaoui <18753008708@sina.cn>
2019-03-18 16:03:06 +0800

    1

--- src/views/layout/list.vue
+++ src/views/layout/list.vue
@@ -11,7 +11,7 @@
     </el-table-column>
     <el-table-column prop="address" label="地址" show-overflow-tooltip>
     </el-table-column>
-    <el-table-column label="操作">
+    <el-table-column label="操作"  width="220">
       <template slot-scope="scope">
         <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
         <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
